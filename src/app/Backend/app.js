79eb644ccaa35b16ae92339121aa85999e2681e4
Raihan <raihansaladin001@gmail.com>
2024-11-30 00:58:18 +0800

    menginstal beberapa untuk keperluan depencies

--- src/app/Backend/app.js
+++ src/app/Backend/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
    res.send('Hello World!!');
 })
 
 app.post('/tes', (req, res) => {
-   const data = req.body;
+   console.log({ requestFromOutside: req.body })
+   const data = req.body.username;
    res.send('tes-api');
 })
 
