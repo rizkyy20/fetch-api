0342097152551bd348a6ce9ccb863b2dc1304b3e
Raihan <raihansaladin001@gmail.com>
2024-11-30 19:59:04 +0800

    membuat crud

--- src/app/Backend/routes/UserRoutes.js
+++ src/app/Backend/routes/UserRoutes.js
@@ -13,6 +13,6 @@ router.get('/users', getUsers);
 router.get('/users/:id', getUserById);
 router.post('/users', createUser);
 router.patch('/users/:id', updateUser);
-router.delete('/users', deleteUser);
+router.delete('/users/:id', deleteUser);
 
 export { router };
