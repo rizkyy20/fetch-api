a9a308d9b71eb8869cfaa00daa6fa2c952d7680d
Raihan <raihansaladin001@gmail.com>
2024-11-30 08:18:40 +0800

    memperbaiki error

--- src/app/Backend/app.js
+++ src/app/Backend/app.js
@@ -1,16 +1,23 @@
-const express = require('express');
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { router as UserRoute } from './routes/UserRoutes.js';
+//const express = require('express');
+//const bodyParser = require('body-parser');
 const app = express();
 const port = 3001;
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.json())
+app.use(cors());
+app.use(express.json());
+app.use(UserRoute);
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
    res.send('Hello World!!');
 })
 
 app.post('/tes', (req, res) => {
-   console.log({ requestFromOutside: req.body })
+   //console.log({ requestFromOutside: req.body })
    const data = req.body.username;
    res.send('tes-api');
 })
