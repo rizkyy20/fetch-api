9aaf4681240ae5c727dce2c9351a4e6d4aebc556
Raihan <raihansaladin001@gmail.com>
2024-12-02 15:11:15 +0800

    membuat api login, register, dan logout

--- src/app/Backend/index.js
+++ src/app/Backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import db from "./config/Database.js";
 import router from "./routes/index.js";
 dotenv.config();
@@ -14,6 +15,7 @@ try {
     console.error(error);
 }
 
+app.use(cors({ credentials:true, origin:'http://localhost:3000' }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
